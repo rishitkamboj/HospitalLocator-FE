4b7f8b001dae905e8da1db007dd53b4b2299f483
rishitkamboj <rishit242@gmail.com>
2024-08-07 23:20:23 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66b3646b8e4ae093199773qad0d900b";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-cloud-user8.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
